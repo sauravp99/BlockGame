45b15805c99b88772499df4a9b272a91d4d2e96a
sauravp <saurav.pa99@gmail.com>
2019-12-30 22:42:52 +0100

    Added Animations for objects and restructering of start level scene

---
+++ FirstGame/Assets/Scripts/SlideDoors.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideDoors : MonoBehaviour{
+    
+    private void OnTriggerEnter(Collider other) {
+        
+    }
+}
