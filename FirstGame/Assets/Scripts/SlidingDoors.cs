16fee68ab85c2822ecddc6a402efa81befe5f591
sauravp <saurav.pa99@gmail.com>
2020-01-04 21:07:31 +0100

    Additional animations applied: Automated sliding doors

---
+++ FirstGame/Assets/Scripts/SlidingDoors.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SlidingDoors : MonoBehaviour{
+    
+    [SerializeField] private Animator animControllerR, animControllerL;
+    
+        // private void
+    private void OnTriggerEnter(Collider other) {
+        
+        if(other.GetComponent<Collider>().tag == "Player"){
+            animControllerL.SetBool("openDoor", true);
+            animControllerR.SetBool("openDoor", true);
+            // animControllerL.Play("lDoorOpen"); 
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+
+        if(other.GetComponent<Collider>().tag == "Player"){
+            animControllerL.SetBool("openDoor", false);
+            animControllerR.SetBool("openDoor", false);
+        }
+    }
+}
---
+++ FirstGame/Assets/Scripts/TPCameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+public class TPCameraFollow : MonoBehaviour{
+
+    public Transform player;
+    public float distanceFromObject = 8f;
+    public Vector3 offset;
+
+    void Update(){
+
+        Vector3 lookOnObject = player.position - transform.position;
+
+        transform.forward = lookOnObject.normalized;
+        
+        Vector3 playerLastPosition = player.position - lookOnObject.normalized * distanceFromObject;
+        
+        playerLastPosition.y = player.position.y + distanceFromObject/4;
+        
+        transform.position = playerLastPosition;    
+    }
+}
