5f5e9413bbb72d794d20dedfbe85630869615ba1
sauravp <saurav.pa99@gmail.com>
2020-04-07 00:51:14 +0200

    Added asset pack: Volumetric Lines

--- FirstGame/Assets/Scripts/PlayerShooting.cs
+++ FirstGame/Assets/Scripts/PlayerShooting.cs
@@ -27,6 +27,9 @@ public class PlayerShooting : MonoBehaviour
         if(Physics.Raycast(shootingPoint.position,shootingPoint.forward,out cast, range)){
             Debug.Log(cast.transform.name);
 
+            GameObject laser = GameObject.Instantiate(m_shotPrefab, transform.position, transform.rotation) as GameObject;
+            laser.GetComponent<ShotBehavior>().setTarget(hit.point);
+            
             Enemy enemy = cast.transform.GetComponent<Enemy>();
             if(enemy != null){
                 enemy.ReduceHealth(damage);
@@ -35,3 +38,48 @@ public class PlayerShooting : MonoBehaviour
 
     }
 }
+
+// using System.Collections;
+
+// public class PlayerShooting : MonoBehaviour
+// {
+//     public float shootRate;
+//     private float m_shootRateTimeStamp;
+
+//     public GameObject m_shotPrefab;
+
+//     RaycastHit hit;
+//     float range = 1000.0f;
+
+
+//     void Update()
+//     {
+
+//         if (Input.GetMouseButton(0))
+//         {
+//             if (Time.time > m_shootRateTimeStamp)
+//             {
+//                 shootRay();
+//                 m_shootRateTimeStamp = Time.time + shootRate;
+//             }
+//         }
+
+//     }
+
+//     void shootRay()
+//     {
+//         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//         if (Physics.Raycast(ray, out hit, range))
+//         {
+//             GameObject laser = GameObject.Instantiate(m_shotPrefab, transform.position, transform.rotation) as GameObject;
+//             laser.GetComponent<ShotBehavior>().setTarget(hit.point);
+//             GameObject.Destroy(laser, 2f);
+
+
+//         }
+
+//     }
+
+
+
+// }
