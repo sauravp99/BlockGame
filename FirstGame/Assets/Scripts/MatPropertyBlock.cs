91b234e556da4d458f8f1c0ae490d17acbea3c43
sauravp99 <saurav.pa99@gmail.com>
2020-05-05 23:42:57 +0200

    add and import asset: Standard assets

--- FirstGame/Assets/Scripts/MatPropertyBlock.cs
+++ FirstGame/Assets/Scripts/MatPropertyBlock.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MatPropertyBlock : MonoBehaviour {
 
-    public Color Color1, Color2;
+    public Color Color1;
     public float Speed = 1, Offset;
     private Renderer _renderer;
     public bool colorChange = false;
@@ -28,8 +28,8 @@ public class MatPropertyBlock : MonoBehaviour {
         _renderer.GetPropertyBlock(_propBlock);
         // Assign our new value.
 
-        // Cycle color: Color.Lerp(Color1, Color2, (Mathf.Sin(Time.time * Speed + Offset) + 1) / 2f)
-        _propBlock.SetColor("_Color", Color.blue);
+        // Cycle color: Color.Lerp(Color.green, Color2, (Mathf.Sin(Time.time * Speed + Offset) + 1) / 2f)
+        _propBlock.SetColor("_Color", Color.green);
         // Apply the edited values to the renderer.
         _renderer.SetPropertyBlock(_propBlock);
     }
