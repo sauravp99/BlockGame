2c9b8214e542e396b64dc0ba052c6de7509bbb2c
sauravp99 <saurav.pa99@gmail.com>
2020-06-01 22:03:09 +0200

    water physics change

--- FirstGame/Assets/Scripts/interactPlants.cs
+++ FirstGame/Assets/Scripts/interactPlants.cs
@@ -5,13 +5,26 @@ public class interactPlants : MonoBehaviour {
     private int timesPoured = 0;
     // private bool bucketEmpty = false;
     public GameObject renderedWater;
+    private float timer;
 
     void Update() {
         
-
     }
-    public void pourWater() {
+    private void FixedUpdate() {
+
+        renderedWater.SetActive(true);
+        timer += Time.deltaTime;
         
+        if(timer > 1) { 
+             
+            timer -= 1;
+
+         renderedWater.transform.position = renderedWater.transform.position + new Vector3(0,-0.05f,0);
+        }    
+    }
+
+    public void pourWater() {
+
         renderedWater.SetActive(true);
         timesPoured++;
         renderedWater.transform.position = renderedWater.transform.position + new Vector3(0,-0.05f,0);
