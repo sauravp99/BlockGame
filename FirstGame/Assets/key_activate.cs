1385e29f2091d1b50f098605763c3c69372a6c64
sauravp99 <saurav.pa99@gmail.com>
2020-06-06 19:02:55 +0200

    add key activation

---
+++ FirstGame/Assets/key_activate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key_activate : MonoBehaviour {
+
+    private Color change_color = new Color(1, 0.92f, 0.016f, 1); 
+    MeshRenderer myRenderer;
+
+    void Start() {
+
+        myRenderer = GetComponent<MeshRenderer>();
+        myRenderer.material.color = change_color;    
+    }
+}
