52753a6a763f11ac3ca4a79182301fec6e26249d
sauravp99 <saurav.pa99@gmail.com>
2020-06-07 22:51:59 +0200

    add water_well script

---
+++ FirstGame/Assets/Scripts/water_well.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class water_well : MonoBehaviour {
+    
+    public GameObject bucket;
+    public GameObject player;
+    public HUD_script hud;
+    void Update() {
+
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+        if (other.gameObject.tag == "Player") {
+            
+            checkCollided();
+        }
+    }
+    void checkCollided() {
+
+        if (player.GetComponent<PlayerCollision>().bucketEquipped) {
+            
+            hud.equipMessage("Press E to fill bucket", true); 
+        }
+    }
+}
