c6bebf85004c90723554ebe25e50deba2b7df1b3
sauravp99 <saurav.pa99@gmail.com>
2020-06-07 23:42:28 +0200

    tidy code

--- FirstGame/Assets/Scripts/water_well.cs
+++ FirstGame/Assets/Scripts/water_well.cs
@@ -5,40 +5,16 @@ public class water_well : MonoBehaviour {
     public GameObject bucket;
     public GameObject player;
     public HUD_script hud;
-    bool contact = false;
-    public bool fillBucket = false;
-    void Update() {
-
-        checkCollided();
-        
-    }
 
     void OnTriggerEnter(Collider other) {
         
         if (other.gameObject.tag == "Player") {
             
-            print("player");
-            contact = true;
-        }
-    }
-    void OnTriggerExit(Collider other) {
+            if (player.GetComponent<PlayerCollision>().bucketEquipped) {
 
-        if (other.gameObject.tag == "Player") {
-            
-            print("player out");
-            contact = false;
-        }    
-    }
-    void checkCollided() {
-
-        hud.equipMessage("Press E to fill bucket", true); 
-
-        if (contact && player.GetComponent<PlayerCollision>().bucketEquipped) {
-            
-            print("display message");
-            hud.equipMessage("Press E to fill bucket", true); 
-            fillBucket = true;
-            bucket.GetComponent<interactPlants>().fillWater();
+                print("Display message");
+                bucket.GetComponent<interactPlants>().fillWater();
+            }
         }
     }
 }
